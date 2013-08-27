7ed838fc0dc8071de711e9f81e0e93ef6c45ce15
tcate <tcate@fileserv.cogdis>
2013-08-27 15:26:23 -0500

    added alarms for ping and freespace

--- cookbooks/web-frontend/recipes/monitoring.rb
+++ cookbooks/web-frontend/recipes/monitoring.rb
@@ -78,12 +78,17 @@ end
 
 cloud_monitoring_alarm  "ping alarm" do
   check_label           'ping'
-  example_id            'remote.ping_packet_loss'
+  criteria              "if (metric['available'] < 80) { return new AlarmStatus(CRITICAL, 'Packet loss is greater than 20%'); } if (metric['available'] < 95) {return new AlarmStatus(WARNING, 'Packet loss is greater than 5%');}return new AlarmStatus(OK, 'Packet loss is normal');"
   notification_plan_id  'npTechnicalContactsEmail'
   action :create
 end
 
-
+cloud_monitoring_alarm  "freespace" do
+  check_label           'filesystem'
+  criteria              "if (metric['avail'] > 2097000) { return new AlarmStatus(OK, 'More than 2GB of free space left.'); } if (metric['avail'] < 1048500) { return new AlarmStatus(CRITICAL, 'Less than 1GB of free space left.'); } if (metric['avail'] < 2097000 ) { return new AlarmStatus(WARNING, 'Less than 2GB of free space left.'); }" 
+  notification_plan_id  'npTechnicalContactsEmail'
+  action                :create
+end
 
 
 
